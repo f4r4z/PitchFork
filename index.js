01372c03789b0a917af4237ab51cb99b4743d82d
siddhantshah-apx <siddhant_shah@comcast.com>
2017-09-23 12:18:31 -0400

    added hardcoded responses

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var Alexa = require('alexa-sdk');
+
+const languageStrings = {
+    'en': {
+        translation: {
+            RESTAURANT_NAMES: 'Starbucks, Saffron Grill, Kushi Tsuru',
+            SKILL_NAME: 'Space Facts',
+            GET_LOCATION_MESSAGE: "As a robot I never go hungry but here are some restaurants that are open near you: ",
+            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            HELP_REPROMPT: 'What can I help you with?',
+            STOP_MESSAGE: 'Goodbye!',
+        },
+    }
+};
+
 const handlers = {
     //2 things: take us to options to ask it
     'getLocations': function () {
-        this.emit(':ask', 'Lets scarf some burgers');
+        const speechOutput = this.t('GET_LOCATION_MESSAGE') + this.t('RESTAURANT_NAMES');
+        this.emit(':ask', speechOutput);
     },
 
     'getMerchantInfo': function () {
@@ -10,11 +25,26 @@ const handlers = {
     }
 };
 
+//start time formatter
+var timeGet = function() {
+    var options = {
+        hour: "2-digit",
+        minute: "2-digit"
+    }
+    var rawTime = new Date();
+    var timeString = rawTime.toLocaleTimeString("en-us",options);
+    var spaceless = timeString.split(" ");
+    var timeToGetLit = spaceless[0];
+    return timeToGetLit
+};
+
 const APP_ID = 'amzn1.ask.skill.ef314947-968e-4afc-8047-8d3908b59470';
 
 exports.handler = function(event, context, callback){
     const alexa = Alexa.handler(event, context, callback);
     alexa.APP_ID = APP_ID;
+
+    alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
