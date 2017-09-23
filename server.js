4a3916bb4bc72ea718bc3f1c4e87ce307d3ef14d
CashCandrien <Ashley_Andrien@comcast.com>
2017-09-23 10:17:25 -0400

    basic connection to devhacks

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ var rp = require('request-promise');
 var app = express();
 var router = express.Router();
 var options = {
-    uri: 'some link',
+    uri: 'https://api.devexhacks.com',
     headers: {
-        'Content-Type': 'applicaiton/json'
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwicGNrIjoxLCJhbGciOiJkaXIiLCJ0diI6Miwia2lkIjoiYTdxIn0..Q8EPUTo189PyagVaeXKw9XgvYN1pEz5Vgp1bgF4Hj9TE2anFkmGILcf7UX9iO6L0cUTgJQm3blatkUZUyUKc6cHFyyuVPKmtZDIU2zmP6VEhxmroUfeqh8YJnOEw9LRVKU1Pq4fVRuZMsIM1Mf6F2oMOAFL8JTw7AK4CQVUWtti4KHaNBtDX9cHOuwRtDbKhQbmySLP0g5ENzrC9gWMLprmq66hX5bI4TAiF2f7KlgjtT9lvph9pLyDsfBhtOanWj6gVmYMqxcNQlUHcgtsH3nlthX1PsOKQppDtmS09hPELzTxEn2kxk2btJ0KPy2iQFQyDSWfER1xgJnFDASr1sg8MNeQh3Qjmp4vuruQMimu1IFVvb1cIsIDS7cWPCUPa2UFYz9YfW1uXVnUpOyZTCWZ3E28YL70Rn2TbP4Hw030rgBWF5Ok1YD51e7BWJXXCq1lIWUG85WmjWZ5Il4nVNZBxBFDPR7lQMG2Gw36ibffzfTDwwHfWhlpkmbqtRLawKEVtYNDcpIvocujQJFHlwCRJ9uex5BXJzQQ6Mrp1cvxp3sp65mU5EPSU4J1OK0Iuj8Yv.I3YRuEIDtnqtHjjjrb9OK0C'
     },
     json: true
 }
@@ -22,23 +23,6 @@ router.use(function(req, res, next){
     next();
 });
 
-router.get('/', function(req, res) {
-    res.send('homepage works');
+rp(options).then(function() {
+    console.log('your shit works.');
 });
-
-router.get('/gimmeData', function(req, res) {
-
-    rp('https://nxg-order-service-int.u1.app.cloud.comcast.net/api/v1/nxgorder/order/502/activation/info').then(function(response) {
-        // take the request, add the client ID and the secret and make an API call to get the token,
-        // res.status(response.statusCode);
-        // once you get the token send it back to the client
-
-        res.send(response);
-        console.log('success');
-    }).catch(function(err){
-        // otherwise say it failed
-        res.send(err);
-        console.log('fail');
-    });
-})
-
